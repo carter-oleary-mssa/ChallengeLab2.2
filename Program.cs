bb90e1656e4bf1d181fff497ecdc6ab417ba3b02
carter-oleary <carter.m.oleary@outlook.com>
2025-04-16 13:38:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string validUsername = "admin";
+string validPassword = "password";
+
+int tries = 3;
+while(true)
+{
+    Console.Write("Enter username: ");
+    string userid = Console.ReadLine();
+    Console.Write("Enter password: ");
+    string password = Console.ReadLine();
+
+    if(validPassword ==  password && validUsername == userid)
+    {
+        Console.WriteLine("Successful login!");
+        break;
+    }
+    else
+    {
+        tries--;
+        Console.WriteLine("Invalid userid or password");
+        Console.WriteLine($"{tries} tries remaining");
+        if(tries == 0)
+        {
+            break;
+        }
+    }
+}
